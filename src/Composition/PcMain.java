2203fc848030992ac971993809e63a6890a6ea18
Kermo Keerma <32697937+SyncMaster7@users.noreply.github.com>
2023-01-19 00:46:30 +0200

    Composition

--- src/Composition/PcMain.java
+++ src/Composition/PcMain.java
@@ -5,7 +5,7 @@ public class PcMain {
     public static void main(String[] args) {
 
         Dimensions dimensions = new Dimensions(45, 30, 22);
-        Case theCase = new Case("220B", "Dell", "400W", dimensions);
+        Case theCase = new Case("DeepCool", "Dell", "400W", dimensions);
         Monitor theMonitor = new Monitor("27-inch", "Samsung", 27, new Resolution(2540, 1440));
         Motherboard theMotherboard = new Motherboard("BJ-219", "MSI", 8, 6, "v2.61");
 
