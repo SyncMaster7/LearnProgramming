4c4a46ac9ea94cedce135cb202a74c8cdf074fec
Kermo Keerma <32697937+SyncMaster7@users.noreply.github.com>
2023-01-18 10:51:55 +0200

    Initial commit

---
+++ src/MethodOverloading/BankAccount.java
@@ -0,0 +1,60 @@
+public class BankAccount {
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmail;
+    private String customerNumber;
+
+    public void deposit(double depositAmount) {
+        this.balance += depositAmount;
+    }
+
+    public void withdrawal(double withdrawalAmount) {
+        if (this.balance - withdrawalAmount < 0) {
+            System.out.println("Only " + this.balance + " available. Withdrawal not processed.");
+        } else {
+            this.balance -= withdrawalAmount;
+            System.out.println("Withdrawal processed. Remaining balance is " + this.balance);
+        }
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(String customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+}
