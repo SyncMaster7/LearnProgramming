8f39561287cc3a3fa6fbbb0e26d290c8d63de305
Kermo Keerma <32697937+SyncMaster7@users.noreply.github.com>
2023-01-19 16:28:39 +0200

    Arrays sorting

---
+++ src/Arrays/ArraysMain.java
@@ -0,0 +1,96 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ArraysMain {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int[] myIntegers = getIntegers();
+        int[] sortedDsc = sortArrayDsc(myIntegers);
+        int[] sortedAsc = sortArrayAsc(myIntegers);
+
+        System.out.println("\n===== Initial array of integers =====");
+        printArray(myIntegers);
+        System.out.println("\n===== Sorted array of integers, 'DSC' order =====");
+        printArray(sortedDsc);
+        System.out.println("\n===== Sorted array of integers, 'ASC' order =====");
+        printArray(sortedAsc);
+    }
+
+    public static int getArrayLength() {
+        System.out.println("How many numbers would you like to insert?");
+        int capacity = 0;
+        int userInput = sc.nextInt();
+
+            if (sc.nextInt() > 0 && sc.nextInt() <= 100) {
+                capacity = userInput;
+            }
+        return capacity;
+    }
+
+    public static int[] getIntegers() {
+        System.out.println("How many numbers would you like to insert?");
+        int capacity = sc.nextInt();
+
+        int[] array = new int[capacity];
+        System.out.println("Enter " + capacity + " integer values:\r");
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Array index " + i + " contents is " + array[i]);
+        }
+    }
+
+    public static int[] sortArrayDsc(int[] array) {
+        int[] sortedArrayDsc = new int[array.length];
+
+        System.arraycopy(array, 0, sortedArrayDsc, 0, array.length);
+        boolean flag = true;
+        int temporaryNumber;
+
+        while (flag) {
+            flag = false;
+
+            for (int i = 0; i < sortedArrayDsc.length - 1; i++) {
+                if (sortedArrayDsc[i] < sortedArrayDsc[i + 1]) {
+                    temporaryNumber = sortedArrayDsc[i];
+                    sortedArrayDsc[i] = sortedArrayDsc[i + 1];
+                    sortedArrayDsc[i + 1] = temporaryNumber;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArrayDsc;
+    }
+
+    public static int[] sortArrayAsc(int[] array) {
+        int[] sortedArrayAsc = new int[array.length];
+
+        System.arraycopy(array, 0, sortedArrayAsc, 0, array.length);
+        boolean flag = true;
+        int temporaryNumber;
+
+        while (flag) {
+            flag = false;
+
+            for (int i = 0; i < sortedArrayAsc.length - 1; i++) {
+                if (sortedArrayAsc[i] > sortedArrayAsc[i + 1]) {
+                    temporaryNumber = sortedArrayAsc[i];
+                    sortedArrayAsc[i] = sortedArrayAsc[i + 1];
+                    sortedArrayAsc[i + 1] = temporaryNumber;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArrayAsc;
+    }
+}
